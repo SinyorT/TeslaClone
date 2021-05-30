2e992e4777ac7b4b427e590880a632cfc2ceff07
Sinyor <abdurahmanov.togrul@gmail.com>
2021-05-31 02:16:04 +0300

    Build Right Burger Menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu';
- 
+import CloseIcon from '@material-ui/icons/Close';
 
 function Header() {
+
+    const [menuStatus,SetMenuStatus]=useState(false)
+
+
     return (
         <Container>
             <a>
@@ -19,7 +23,20 @@ function Header() {
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
             </RightMenu>
-            <CustomMenu />
+            <CustomMenu onClick={() =>SetMenuStatus(true) }/>
+            <BurgerNav show={menuStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={() =>SetMenuStatus(false) }/>
+                </CloseWrapper> 
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadaster</a></li>
+                <li><a href="#">Roadaster</a></li>
+                <li><a href="#">Roadaster</a></li>
+                <li><a href="#">Roadaster</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -36,6 +53,7 @@ const Container = styled.div`
     top:0;
     left:0;
     right:0;
+    z-index:1;
 `;
 
 const Menu = styled.div`
@@ -67,4 +85,36 @@ const RightMenu = styled.div`
 
 const CustomMenu = styled(MenuIcon)`
    cursor:pointer;
+`;
+
+const BurgerNav = styled.div`
+    position:fixed;
+    top:0;
+    bottom:0;
+    right:0;
+    background:white;
+    width:300px;
+    z-index:100;
+    list-style:none;
+    padding:20px;
+    display:flex;
+    flex-direction:column;
+    text-align:start;
+    transform:${props=>props.show ? 'translateX(0)':'translateX(100%)'};
+    transition:transform 0.2s ease-in;
+        li{
+            padding:15px 0;
+            border-bottom:1px solid rgba(0,0,0,.2);
+            a{
+                font-weight:600;
+            }
+        }
+`;
+const CustomClose = styled(CloseIcon)`
+        cursor:pointer;
+`;
+
+const CloseWrapper = styled.div`
+    display:flex;
+    justify-content:flex-end    
 `;
